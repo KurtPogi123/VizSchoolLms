494c8f6d03052f56701e7b7c6cdd297fc10243e3
Kurt-vizserve <kurt.vizserve@gmail.com>
2025-08-12 11:31:08 +0800

    first push

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello React + Tailwind + Vite!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
